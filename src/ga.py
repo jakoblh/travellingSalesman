3226c3dac5e5e46be601655c609dda860626b66d
jakoblh <jakob.lamberthartmann.canada@gmail.com>
2019-02-24 01:06:42 +0100

    - bug fixes - exporting graphs as .png

--- src/ga.py
+++ src/ga.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 def rand_points(num):
@@ -12,39 +13,57 @@ def rand_path(num):
 
 
 def evaluate(points, order):
+    if order is None:
+        return None
     res = 0
     last = order[0]
     for curr in order[1:]:
         x = points[int(last)][0] - points[int(curr)][0]
         y = points[int(last)][1] - points[int(curr)][1]
         res += (x**2 + y**2)**.5
+        last = curr
     return res
 
 
-def get_child(parents):
-    res = parents[0][: int(parents[0].size / 2)]
+def get_child(parents, mut_rate):
+    start = random.randint(0, parents[0].size - 1)
+    end = random.randint(start + 1, parents[0].size)
+    res = parents[0][start: end]
     for val in parents[1]:
         if val not in res:
             res = np.append(res, val)
+    if random.uniform(0, 1) < mut_rate:
+        #print("Mutation!")
+        idx = np.random.choice(range(res.size), 2, True)
+        tmp = res[idx[0]]
+        res[idx[0]] = res[idx[1]]
+        res[idx[1]] = tmp
     return res
 
 
-def get_gen(points, prev_gen, gen_size):
+def get_gen(points, prev_gen, gen_size, mut_rate):
     gen = np.empty((0, points.shape[0]))
     avg_len = 0
+    best = None
     if prev_gen is None:
         for i in range(gen_size):
             path = rand_path(points.shape[0])
             gen = np.append(gen, [path], axis=0)
     else:
-        evaluation = np.array([])
+        lengths = np.array([])
         for ind in prev_gen:
-            evaluation = np.append(evaluation, evaluate(points, ind))
-        sum = np.sum(evaluation)
-        avg_len = sum / evaluation.size
-        evaluation = 1 / evaluation
+            lengths = np.append(lengths, evaluate(points, ind))
+        bestidx = np.argmin(lengths)
+        best_length = lengths[bestidx]
+        best = prev_gen[bestidx]
+        avg_len = np.sum(lengths) / lengths.size
+        evaluation = 1 / lengths**4
         evaluation = evaluation / np.sum(evaluation)
+        avg_parent_length = 0
         for i in range(gen_size):
-            parents = prev_gen[np.random.choice(prev_gen.shape[0], 2, False, evaluation)]
-            gen = np.append(gen, [get_child(parents)], axis=0)
-    return gen, avg_len
+            parentidx = np.random.choice(prev_gen.shape[0], 2, False, evaluation)
+            avg_parent_length += np.sum(lengths[parentidx])
+            parents = prev_gen[parentidx]
+            gen = np.append(gen, [get_child(parents, mut_rate)], axis=0)
+        print("avg parent len: {0}".format(avg_parent_length / (gen_size * 2)))
+    return gen, avg_len, best
--- src/main.py
+++ src/main.py
@@ -1,16 +1,49 @@
 from src.ga import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
-iterations = 50
-gen_size = 50
-points = rand_points(50)
+iterations = 100
+gen_size = 100
+#points = np.array([(6, 0), (3, 1), (1, 2), (0, 3), (0, 4), (1, 5), (3, 6), (6, 7)])
+points = rand_points(10)
+mut_rate = .1
 
 
 def find_path():
     prev_gen = None
     for i in range(iterations):
-        cur_gen, avg_len = get_gen(points, prev_gen, gen_size)
-        print("{0} avg len: {1}".format(i, avg_len))
-        prev_gen = cur_gen
+        prev_gen, avg_len, best = get_gen(points, prev_gen, gen_size, mut_rate)
+        if best is not None:
+            draw_graph(best, i)
+        print("{0} avg len: {1}\n  best len:{2}".format(i, avg_len, evaluate(points, best)))
 
 
+def draw_graph(ind, index):
+    G = nx.Graph()
+    prev = ind[0]
+    nodes = dict()
+    for i in range(points.shape[0]):
+        nodes[i] = points[i]
+    G.add_nodes_from(nodes.keys())
+    for i in ind[1:]:
+        p1 = int(prev)
+        p2 = int(i)
+        G.add_edge(p1, p2)
+        prev = i
+    nx.draw(G, nodes)
+    plt.savefig("imgs/graph{0}.png".format(index), format="PNG")
+    plt.clf()
+
+
+'''
+p1 = rand_path(10)
+p2 = rand_path(10)
+print(p1)
+print(p2)
+print(get_child([p1, p2], .2))
+'''
 find_path()
+'''
+p1 = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+print(evaluate(points, p1))
+'''
